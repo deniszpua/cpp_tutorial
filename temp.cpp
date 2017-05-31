2ae8e6a264495c265611cc02c1d58f2bf6d2379b
Denys <quux@ukr.net>
2017-05-31 13:37:32 +0300

    Output was formatted

--- temp.cpp
+++ temp.cpp
@@ -1,5 +1,6 @@
 // temp.cpp: Denys Soroka
 // Description: a program that prints the immortal saying "Hello World"
+// and demonstrates some output formatting flags
 
 #include <iostream>
 using namespace std;
@@ -7,6 +8,14 @@ using namespace std;
 /** Main docstring */
 int main()
 {
-  cout << "Hello World!" << endl;
+  cout.setf(ios::left);
+  for (size_t line = 0; line < 6; line++) {
+    for (size_t i = 0; i < 4; i++) {
+      cout.width(17);
+      cout << "Hello World!";
+      
+    }
+    cout << endl;
+  }
   return 0;
 }
