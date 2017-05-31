2d0c2e0ce90eb3e5b315ff4b9cf856fbc0e3b7d4
Denys <quux@ukr.net>
2017-05-31 12:58:33 +0300

    Comments and docstring examples were added

--- temp.cpp
+++ temp.cpp
@@ -1,7 +1,10 @@
+// temp.cpp: Denys Soroka
+// Description: a program that prints the immortal saying "Hello World"
+
 #include <iostream>
 
 using namespace std;
-
+/** Main docstring */
 int main()
 {
   cout << "Hello World!" << endl;
