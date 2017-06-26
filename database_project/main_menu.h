ef4c130b0c5ccb74b812a1a156ded59a0cb8a96e
Denys <quux@ukr.net>
2017-06-26 23:15:48 +0300

    Database project headers and tests were added

---
+++ database_project/main_menu.h
@@ -0,0 +1,15 @@
+// main_menu.h. Denys Soroka
+// represents database project main menu
+
+/*
+ * Prints following menu and handles input:
+ *
+ *  Composer Database
+ * ---------------------------------------------
+ *  1) Add a new composer
+ *  2) Retrieve a composer's data
+ *  3) Promote/demote a composer's rank
+ *  4) List all composers
+ *  5) List all composers by rank
+ *  0) Quit
+ */
